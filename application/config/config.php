ac78ceeb69d6e25685224526be5a4fd8df45bd7c
Fikri Attamami Laguliga <sabarorang@gmail.com>
2019-05-29 10:20:27 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:80/digital-touch-point/';
+$config['base_url'] = 'http://10.14.36.136/digitouchpoint_beta/';
 
 /*
 |--------------------------------------------------------------------------
